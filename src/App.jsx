48fd3ad1c761e6a13b650c610bd5edebe3df08c1
shaikot07 <saifulislamshaikat007@gmail.com>
2024-02-13 00:07:36 +0600

    setup project for testing multer

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+         <h2 className='text-3xl'>Testing and intregation multer</h2>
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
